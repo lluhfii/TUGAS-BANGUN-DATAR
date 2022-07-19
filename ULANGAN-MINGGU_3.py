65fb2bc1f04f57803422f1d9552b659f6d7e6e52
ChoNg-YuN? <107825620+lluhfii@users.noreply.github.com>
2022-07-19 11:09:15 +0700

    Add files via upload

---
+++ ULANGAN-MINGGU_3.py
@@ -0,0 +1,89 @@
+input('Nama : ')
+input('Kelas : ')
+
+print('=====================================================================')
+print('============================= QUESTIONS =============================')
+print('=====================================================================')
+print('Question 1')
+print('Diketahui sisi = 6, sebutkan nama dan hitunglah luas bangun datar tersebut!')
+
+bd = input('Nama bangun datar : ')
+si1 = eval(input('Sisi : '))
+si2 = eval(input('Sisi : '))
+hasil = si1*si2
+print("Bangun datar adalah ",bd, "dan Luas bangun datar tersebuat adalah ", hasil)
+print('')
+print('')
+print('')
+
+
+
+print('Question 2')
+print('Diketahui alas = 7, tinggi = 8, Sebutkan nama dan hitunglah Luas bangun datar tersebut!')
+bd2 = input('Nama bangun datar : ')
+aa = eval(input('Alas : '))
+t = eval(input('Tinggi : '))
+hasil2 = (aa*t) /2
+print('Bangun datar adalah ', bd2, 'dan Luas bangun datar tersebuat adalah ',hasil2)
+print('')
+print('')
+print('')
+
+
+print('Question 3')
+print('Diketahui panjang = 4, lebar = 9, Sebutkan nama dan hitunglah Luas bangun datar tersebut!')
+bd3 = input('Nama bangun datar : ')
+p = eval(input('Panjang : '))
+l = eval(input('Lebar : '))
+hasil3 = (p*l)
+print('Bangun datar adalah ', bd3, 'dan Luas bangun datar tersebuat adalah ',hasil3)
+print('')
+print('')
+print('')
+
+
+print('Question 4')
+print('Diketahui jari-jari = 56, Sebutkan nama dan hitunglah Luas bangun datar tersebut!')
+bd4 = input('Nama bangun datar : ')
+rr = float(input('rr : '))
+r = float(input('r : '))
+hasil4 = (rr*r)
+print('Bangun datar adalah ', bd4, 'dan Luas bangun datar tersebuat adalah ',hasil4)
+print('')
+print('')
+print('')
+
+print('Question 5')
+print('Diketahui Diagonal I = 4, Diagonal II = 8 Sebutkan nama dan hitunglah Luas bangun datar tersebut!')
+bd5 = input('Nama bangun datar : ')
+d1 = float(input('Diagonal I : '))
+d2 = float(input('Diagonal II : '))
+hasil5 = (d1*d2) /2
+print('Bangun datar adalah ', bd5, 'dan Luas bangun datar tersebuat adalah ',hasil5)
+print('')
+print('')
+print('')
+
+print('Question 6')
+print('Diketahui sisi AB = dan CD sejajar, sisi AB = 6, sisi CD = 8, tinggi = 3 Sebutkan nama dan hitunglah Luas bangun datar tersebut!')
+bd6 = input('Nama bangun datar : ')
+siab = float(input('Sisi AB : '))
+sicd = float(input('Sisi CD : '))
+t2 = float(input('Tinggi : '))
+hasil6 = (siab+sicd)*t2/2
+print('Bangun datar adalah ', bd6, 'dan Luas bangun datar tersebuat adalah ',hasil6)
+print('')
+print('')
+print('')
+
+
+print('Question 7')
+print('Diketahui Sebuah jajar genjang dengan alas = 9, tinggi = 7 hitunglah Luas bangun datar tersebut!')
+bd7 = input('Nama bangun datar : ')
+al = float(input('Alas : '))
+ti = float(input('Tinggi : '))
+hasil7 = (al*ti)
+print('Bangun datar adalah ', bd7, 'dan Luas bangun datar tersebuat adalah ',hasil7)
+print('')
+print('')
+print('')
